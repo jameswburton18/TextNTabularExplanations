2ae9425dd6765de183206da00534806ae3f54c2e
James Burton <james.burton@durham.ac.uk>
2023-03-08 12:00:27 +0000

    Added training on ncc

--- scripts/batch_train.sh
+++ scripts/batch_train.sh
@@ -1,8 +1,19 @@
-for idx in 0 1 # {7..10}
+for idx in 0 # {7..10}
 do
     for cfg in airbnb_${idx}
     do
-        sbatch --job-name=$cfg scripts/train_office_pc.sh $cfg
+        # sbatch --job-name=$cfg scripts/train_office_pc.sh $cfg
+        sbatch --job-name=$cfg scripts/train.sh $cfg
+        sleep 4 # so that wandb runs don't get assigned the same number
+    done
+done
+
+for idx in 0 # {7..10}
+do
+    for cfg in books_${idx}
+    do
+        # sbatch --job-name=$cfg scripts/train_office_pc.sh $cfg
+        sbatch --job-name=$cfg scripts/train.sh $cfg
         sleep 4 # so that wandb runs don't get assigned the same number
     done
 done
