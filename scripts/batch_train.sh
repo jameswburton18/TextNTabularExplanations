6243391070eb16e0f3797c5099427232cbcfbaa3
James Burton <james.burton@durham.ac.uk>
2023-03-16 11:01:21 +0000

    imdb genre

--- scripts/batch_train.sh
+++ scripts/batch_train.sh
@@ -1,19 +1,19 @@
-for idx in 0 # {7..10}
-do
-    for cfg in airbnb_${idx}
-    do
-        # sbatch --job-name=$cfg scripts/train_office_pc.sh $cfg
-        sbatch --job-name=$cfg scripts/train.sh $cfg
-        sleep 4 # so that wandb runs don't get assigned the same number
-    done
-done
+# for idx in 0 # {7..10}
+# do
+#     for cfg in airbnb_${idx}
+#     do
+#         sbatch --job-name=$cfg scripts/train_office_pc.sh $cfg
+#         # sbatch --job-name=$cfg scripts/train.sh $cfg
+#         sleep 4 # so that wandb runs don't get assigned the same number
+#     done
+# done
 
 for idx in 0 # {7..10}
 do
-    for cfg in books_${idx}
+    for cfg in imdb_genre_text_${idx}
     do
-        # sbatch --job-name=$cfg scripts/train_office_pc.sh $cfg
-        sbatch --job-name=$cfg scripts/train.sh $cfg
+        sbatch --job-name=$cfg scripts/train_office_pc.sh $cfg
+        # sbatch --job-name=$cfg scripts/train.sh $cfg
         sleep 4 # so that wandb runs don't get assigned the same number
     done
 done
