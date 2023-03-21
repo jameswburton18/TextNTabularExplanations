f4f2d17b57e7e8de7717430322694569142d8f00
James Burton <james.burton@durham.ac.uk>
2023-03-21 17:04:09 +0000

    scripts

--- scripts/batch_run_exps.sh
+++ scripts/batch_run_exps.sh
@@ -2,8 +2,8 @@ for idx in {1..7}
 do
     for model_name in imdb_genre_${idx}
     do
-        sbatch --job-name=$model_name scripts/run_exps_office_pc.sh $model_name
-        # sbatch --job-name=$cfg scripts/train.sh $cfg
+        # sbatch --job-name=$model_name scripts/run_exps_office_pc.sh $model_name
+        sbatch --job-name=$model_name scripts/run_exps.sh $model_name
         sleep 4 # so that wandb runs don't get assigned the same number
     done
 done
---
+++ scripts/run_exps.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# Instructing SLURM to locate and assign X number of nodes with Y number of
+# cores in each node. X,Y are integers.
+#SBATCH -N 1
+#SBATCH -c 4
+
+# Governs the run time limit and resource limit for the job. See QOS tables for combinations
+# Available QOS: [debug, short, long-high-prio, long-low-prio, long-cpu]
+#SBATCH -p res-gpu-small
+#SBATCH --qos short #long-high-prio
+#SBATCH -t 02-00:00
+# -x shows which ones to ignore
+# #SBATCH -x gpu[0-8] #[7,8,10,11,12]
+# #SBATCH --gres=gpu:ampere:1 #--gres gpu for normal, --gres=gpu:ampere:1 for whole 80gb card
+#SBATCH -x gpu[0-6,10-12] #[7,8,10,11,12]
+#SBATCH --gres gpu #--gres gpu for normal, --gres=gpu:ampere:1 for whole 80gb card
+
+# Job name appears in the squeue output, output is the output filename 
+#SBATCH -o logs/%x-%A.out
+
+# Pick how much memory to allocate per CPU core.
+#SBATCH --mem 8G
+#SBATCH --gres gpu
+
+source ./env/bin/activate
+
+# Commands to be run:
+python --version
+nvidia-smi
+hostname
+echo "Node id: $SLURM_NODEID"
+
+mem=`nvidia-smi --query-gpu=memory.total --format=csv | tail -n 1 | awk '{print $1}'`
+echo "$mem Mb available"
+
+date '+%c'
+# python combine_LIME.py --dataset fraud --model joint
+python src/test_run_exps.py --model_name $1
