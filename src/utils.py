0dd66e43c86944a72d8fd131269086871ec32f5c
James Burton <james.burton@durham.ac.uk>
2023-03-22 15:21:30 +0000

    model name to desc dict

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,20 @@
 import numpy as np
 
+
+MODEL_NAME_TO_DESC_DICT = {
+    'imdb_genre_0': 'all as text',
+    'imdb_genre_1': 'tab as text',
+    'imdb_genre_2': 'tabx5',
+    'imdb_genre_3': 'all as text, reorder1',
+    'imdb_genre_4': 'all as text, reorder2',
+    'imdb_genre_5': 'tabx5_nodesc',
+    'imdb_genre_6': 'tabx2_nodesc',
+    'imdb_genre_7': 'tabx2',
+    'imdb_genre_8': 'tab as text, reorder3',
+    'imdb_genre_9': 'text col only',
+}
+   
+
 def row_to_string(row, cols):
     row['text'] = ' | '.join(f'{col}: {row[col]}' for col in cols)
     return row
@@ -11,7 +26,7 @@ def multiple_row_to_string(row, cols, multiplier=1, nodesc=False):
     return row
 
 def prepare_text(dataset, version):
-    if version == 'all_text':
+    if version == 'all_as_text':
         cols = ['Year','Runtime (Minutes)', 'Rating', 'Votes', 'Revenue (Millions)','Metascore', 'Rank', 'Description']
         dataset = dataset.map(row_to_string, fn_kwargs={'cols': cols})
         return dataset
@@ -43,7 +58,11 @@ def prepare_text(dataset, version):
         cols = ['Description', 'Rank', 'Metascore', 'Revenue (Millions)', 'Votes', 'Rating', 'Runtime (Minutes)', 'Year']
         dataset = dataset.map(row_to_string, fn_kwargs={'cols': cols})
         return dataset
-    elif version == None:
+    elif version == 'reorder3':
+        cols = ['Revenue (Millions)','Metascore', 'Rank', 'Year','Votes','Runtime (Minutes)', 'Rating']
+        dataset = dataset.map(row_to_string, fn_kwargs={'cols': cols})
+        return dataset
+    elif version == 'text_col_only':
         # dataset rename column
         dataset = dataset.rename_column('Description', 'text')
         return dataset
@@ -57,7 +76,13 @@ def format_text_pred(pred):
         return np.array([pred['score'], 1-pred['score']])
     
 def select_prepare_array_fn(model_name):
-    if model_name == 'imdb_genre_1': # tab as text
+    if model_name == 'imdb_genre_0': # all as text
+        cols = ['Year','Runtime (Minutes)', 'Rating', 'Votes', 'Revenue (Millions)','Metascore', 'Rank', 'Description']
+        def array_fn(array):
+            return np.array(
+                " | ".join([f"{col}: {val}" for col, val in zip(cols, array)]), dtype="<U512"
+                )
+    elif model_name == 'imdb_genre_1': # tab as text
         cols = ['Year','Runtime (Minutes)', 'Rating', 'Votes', 'Revenue (Millions)','Metascore', 'Rank']
         def array_fn(array):
             return np.array(
@@ -101,6 +126,13 @@ def select_prepare_array_fn(model_name):
             return np.array(
                 " | ".join([f"{col}: {val}" for col, val in zip(cols, array)]), dtype="<U512"
                 )
-    
+    elif model_name == 'imdb_genre_8': # reorder3
+        cols = ['Revenue (Millions)','Metascore', 'Rank', 'Year','Votes','Runtime (Minutes)', 'Rating']
+        def array_fn(array):
+            return np.array(
+                " | ".join([f"{col}: {val}" for col, val in zip(cols, array)]), dtype="<U512"
+                )
+    else:
+        raise ValueError(f'select_prepare_array_fn not implemented for: {model_name}')
     return array_fn
         
