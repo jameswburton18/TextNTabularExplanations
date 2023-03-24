d54557fb280b4cbcae11ace211379496a86da471
James Burton <james.burton@durham.ac.uk>
2023-03-24 08:29:14 +0000

    Minor

--- scripts/batch_run_exps.sh
+++ scripts/batch_run_exps.sh
@@ -1,9 +1,9 @@
-for idx in {1..7}
+for idx in 8
 do
     for model_name in imdb_genre_${idx}
     do
-        # sbatch --job-name=$model_name scripts/run_exps_office_pc.sh $model_name
-        sbatch --job-name=$model_name scripts/run_exps.sh $model_name
+        sbatch --job-name=$model_name scripts/run_exps_office_pc.sh $model_name
+        # sbatch --job-name=$model_name scripts/run_exps.sh $model_name
         sleep 4 # so that wandb runs don't get assigned the same number
     done
 done
