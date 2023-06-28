e7bae1a862d0e2f9b7767bf218c3990c4db9da42
James Burton <james.burton@durham.ac.uk>
2023-06-28 08:03:02 +0100

    minor

--- scripts/scp.sh
+++ scripts/scp.sh
@@ -1,5 +1,6 @@
-for exp in 0
+for model in bert deberta disbert drob
 do
     # scp imdb_genre_${exp}_shap_values.npy office:CodingProjects/TextNTabularExplanations/
-    scp imdb_genre_${exp}_val_shap_values.npy office:CodingProjects/TextNTabularExplanations/shap_vals/imdb_genre/
+    # scp imdb_genre_${exp}_val_shap_values.npy office:CodingProjects/TextNTabularExplanations/shap_vals/imdb_genre/
+    scp -r models/shap_vals_${model}_sf1/ office:CodingProjects/TextNTabularExplanations/models/
 done
